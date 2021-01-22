e91fa937409faccdd90db3cd651a62c4c993b02f
Cynthia Paola Duro Sanchez <al221811737@gmail.com>
2021-01-22 03:19:05 -0600

    Migración y modelo de Producto

---
+++ database/migrations/2021_01_22_085952_create_productos_table.php
@@ -0,0 +1,45 @@
+
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            
+            $table->id()->comment('id');
+
+            $table->string('clave', 25)->comment('clave');
+            $table->string('producto', 200)->comment('producto');
+
+            $table->unsignedInteger('existencias')->comment('existencias');
+
+            $table->decimal('precio_unitario', 10)->comment('precio unitario');
+
+            $table->enum('unidad_de_medida', ['pieza', 'caja', 'kilogramo', 'metro', 'bolsa', 'otro'] )->comment('unidad_de_medida');
+            $table->enum('estatus', ['activo', 'inactivo'])->comment('estatus');
+
+            $table->timestamps();
+        
+    });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('productos');
+    }
+}
