35f685bd7a85485a2ecb38aa854f18ca3095bd99
DSM51 Jesus Raul Jimenez Esquivel <al221910356@gmail.com>
2021-03-19 22:42:16 -0600

    listado

---
+++ resources/js/productos.js
@@ -0,0 +1,38 @@
+module.exports = {
+    '/api/productos':
+      data = [
+        {
+          id: 1
+          , sabor: 'chocolate'
+          , tamaño: 'grande'
+          , descripcion: '10'
+          , precio: 780
+          , fotografia: 'http://placeimg.com/100/100/people'
+        }
+        , {
+          id: 2
+          , sabor: 'fresa'
+          , tamaño: 'chico'
+          , descripcion: '13'
+          , precio: 300
+          , fotografia: 'http://placeimg.com/100/100/people'
+        }
+        , {
+          id: 3
+          , sabor: 'vainilla'
+          , tamaño: 'mediano'
+          , descripcion:' 12'
+          , precio: 450
+          , fotografia: 'http://placeimg.com/100/100/people'
+        }
+        , {
+          id: 4
+          , sabor: 'chocoflan'
+          , tamaño: 'chico'
+          , descripcion: '13'
+          , precio: 350
+          , fotografia: 'http://placeimg.com/100/100/people'
+        }
+      ]
+}
+    
---
+++ resources/js/usuarios.js
@@ -0,0 +1,54 @@
+module.exports = {
+    '/api/usuarios':
+      data = [
+        {
+          id: 1
+          , rol: 'Administrador'
+          , name: 'Brenda Ximena'
+          , primer_apellido: 'Duro'
+          , segundo_apellido: 'Sanchez'
+          , sexo:'Femenino'
+          , nacimiento:'1999-06-21'
+          , email:'brenda@correo.com'
+          , telefono:'7229076085'
+          , avatar: 'http://placeimg.com/100/100/people'
+        }
+        , {
+          id: 2
+          , rol: 'empleado'
+          , name: 'Jesus Raul'
+          , primer_apellido: 'Jimenez'
+          , segundo_apellido: 'Esquivel'
+          , sexo:'Masculino'
+          , nacimiento:'2000-02-21'
+          , email:'jesus@correo.com'
+          , telefono: '7253469087'
+          , avatar: 'http://placeimg.com/100/100/people'
+        }
+        , {
+          id: 3
+          , rol: 'Empleado'
+          , name: 'Cynthia Paola'
+          , primer_apellido: 'Duro'
+          , segundo_apellido: 'Sanchez'
+          , sexo:'Femenino'
+          , nacimiento:'2000-05-08'
+          , email:'cynthia@correo.com'
+          , telefono:'7223459087'
+          , avatar: 'http://placeimg.com/100/100/people'
+        }
+        , {
+          id: 4
+          , rol: 'Empleado'
+          , name: 'Denni'
+          , primer_apellido: 'Jesus'
+          , segundo_apellido: 'Fernandez'
+          , sexo:'Femenino'
+          , nacimiento:'2000-04-21'
+          , email:'denni@correo.com'
+          , telefono:'7223450909'
+          , avatar: 'http://placeimg.com/100/100/people'
+        }
+      ]
+}
+    
